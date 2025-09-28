88fd27eafa74b9625b6587672c8548f63e178d6c
PitcherDev <cantaro755@gmail.com>
2025-09-28 19:04:34 +0200

    Configuración inicial - primer commit

---
+++ src/main/java/com/motivacion/frasesmotivadoras/FrasesMotivadorasApplication.java
@@ -0,0 +1,13 @@
+package com.motivacion.frasesmotivadoras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrasesMotivadorasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrasesMotivadorasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motivacion/frasesmotivadoras/FrasesMotivadorasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motivacion.frasesmotivadoras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrasesMotivadorasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
